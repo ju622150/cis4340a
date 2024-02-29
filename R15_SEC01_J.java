7c89849b0c284f6c294fa8a5531e8442ba9a2492
ju622150 <161380728+ju622150@users.noreply.github.com>
2024-02-29 13:08:00 -0500

    Update R15_SEC01_J.java
    Compliant Solution

--- R15_SEC01_J.java
+++ R15_SEC01_J.java
@@ -1,10 +1,23 @@
-
-public RandomAccessFile openFile(final java.io.File f) {
-  askUserPermission(f.getPath());
-  // ...
-  return (RandomAccessFile)AccessController.doPrivileged(new PrivilegedAction <Object>() {
-    public Object run() {
-      return new RandomAccessFile(f, f.getPath());
-    }
-  });
+private void privilegedMethod(final String filename)
+                              throws FileNotFoundException {
+  final String cleanFilename;
+  try {
+    cleanFilename = cleanAFilenameAndPath(filename);
+  } catch (/* exception as per spec of cleanAFileNameAndPath */) {
+    // Log or forward to handler as appropriate based on specification
+    // of cleanAFilenameAndPath
+  }
+  try {
+    FileInputStream fis =
+        (FileInputStream) AccessController.doPrivileged(
+          new PrivilegedExceptionAction() {
+        public FileInputStream run() throws FileNotFoundException {
+          return new FileInputStream(cleanFilename);
+        }
+      }
+    );
+    // Do something with the file and then close it
+  } catch (PrivilegedActionException e) {
+    // Forward to handler
+  }
 }
