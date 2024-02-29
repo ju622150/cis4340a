22cbc369435e1aabee966cdc42600c0f89d96e86
ju622150 <161380728+ju622150@users.noreply.github.com>
2024-02-29 13:07:06 -0500

    Update R14_SER04_J.java
    Compliant Solution

--- R14_SER04_J.java
+++ R14_SER04_J.java
@@ -1,50 +1,19 @@
 public final class Hometown implements Serializable {
-  // Private internal state
-  private String town;
-  private static final String UNKNOWN = "UNKNOWN";
+  // ... All methods the same except the following:
  
-  void performSecurityManagerCheck() throws AccessDeniedException {
-    // ...
-  }
- 
-  void validateInput(String newCC) throws InvalidInputException {
-    // ...
-  }
- 
-  public Hometown() {
-    performSecurityManagerCheck();
- 
-    // Initialize town to default value
-    town = UNKNOWN;
-  }
- 
-  // Allows callers to retrieve internal state
-  String getValue() {
-    performSecurityManagerCheck();
-    return town;
-  }
- 
-  // Allows callers to modify (private) internal state
-  public void changeTown(String newTown) {
-    if (town.equals(newTown)) {
-      // No change
-      return;
-    } else { 
-      performSecurityManagerCheck();
-      validateInput(newTown);
-      town = newTown;
-    }
-  }
- 
+  // writeObject() correctly enforces checks during serialization
   private void writeObject(ObjectOutputStream out) throws IOException {
+    performSecurityManagerCheck();
     out.writeObject(town);
   }
  
+  // readObject() correctly enforces checks during deserialization
   private void readObject(ObjectInputStream in) throws IOException {
     in.defaultReadObject();
     // If the deserialized name does not match the default value normally
     // created at construction time, duplicate the checks
     if (!UNKNOWN.equals(town)) {
+      performSecurityManagerCheck();
       validateInput(town);
     }
   }
