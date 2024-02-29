c8c492c4942a94c92af9557204d75eee78d7b704
ju622150 <161380728+ju622150@users.noreply.github.com>
2024-02-29 13:06:36 -0500

    Update R12_TSM01_J.java
    Compliant Solution

--- R12_TSM01_J.java
+++ R12_TSM01_J.java
@@ -1,11 +1,12 @@
+
 final class Publisher {
-  public static volatile Publisher published;
+  static volatile Publisher published;
   int num;
  
   Publisher(int number) {
-    published = this;
     // Initialization
     this.num = number;
     // ...
+    published = this;
   }
 }
